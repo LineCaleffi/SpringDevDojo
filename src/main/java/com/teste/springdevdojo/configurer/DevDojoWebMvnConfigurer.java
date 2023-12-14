62b636d16cae5201800e3e6b8087594cce7151b0
DESKTOP-UV7KBFV\Jeean <alinecaleffi97@gmail.com>
2023-12-14 11:50:29 -0300

    comentarios

--- src/main/java/com/teste/springdevdojo/configurer/DevDojoWebMvnConfigurer.java
+++ src/main/java/com/teste/springdevdojo/configurer/DevDojoWebMvnConfigurer.java
@@ -14,7 +14,14 @@ public class DevDojoWebMvnConfigurer implements WebMvcConfigurer{
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         PageableHandlerMethodArgumentResolver pageHandler = new PageableHandlerMethodArgumentResolver();
-        pageHandler.setFallbackPageable(PageRequest.of(0, 5));
+        pageHandler.setFallbackPageable(PageRequest.of(0, 10));
         resolvers.add(pageHandler);
     }
 }
+
+
+/*
+ * (PageRequest.of(0, 5))
+ * 
+ * Define quantos elementos irão aparecer por página
+ */
