89d5e3d5f6c903c7c1139a6226184711cfe63f5e
DESKTOP-UV7KBFV\Jeean <alinecaleffi97@gmail.com>
2023-12-14 11:47:50 -0300

    ajustes e WebMvcConfigurer

--- src/main/java/com/teste/springdevdojo/request/AnimePutRequestBody.java
+++ src/main/java/com/teste/springdevdojo/request/AnimePutRequestBody.java
@@ -7,8 +7,8 @@ import lombok.Data;
 
 @Data
 public class AnimePutRequestBody {
+    private Long id;
 	@NotEmpty(message="Anime não pode ser vazio")
 	@NotNull(message="Anime não pode ser nulo")
-    private Long id;
     private String name;
 }
