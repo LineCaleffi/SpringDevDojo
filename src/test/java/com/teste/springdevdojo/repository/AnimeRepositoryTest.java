97b0332fa797aa7a16ef4df689ade054ad66c413
DESKTOP-UV7KBFV\Jeean <alinecaleffi97@gmail.com>
2024-02-01 16:14:28 -0300

    JUnit part 2 - Update

--- src/test/java/com/teste/springdevdojo/repository/AnimeRepositoryTest.java
+++ src/test/java/com/teste/springdevdojo/repository/AnimeRepositoryTest.java
@@ -26,6 +26,22 @@ class AnimeRepositoryTest {
         Assertions.assertThat(animeSaved.getName()).isEqualTo(animeSaved.getName());	
     }
 	
+	@Test
+	@DisplayName("Anime ALTERADO com sucesso")
+	void updateSuccess() {
+		Anime anime = createAnime();
+		Anime animeSaved = this.animeRepository.save(anime);
+		
+		animeSaved.setName("Avatar: A Lenda de AANG");
+		
+		Anime animeUpdate = this.animeRepository.save(animeSaved);
+		
+		//  as Assertions servem para testar se o seu código esta de acordo com as regras que deve seguir
+		Assertions.assertThat(animeSaved).isNotNull();
+		Assertions.assertThat(animeSaved.getId()).isNotNull();
+        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeSaved.getName());	
+    }
+	
 
 	private Anime createAnime() {
 		return Anime.builder().name("Naruto").build();
