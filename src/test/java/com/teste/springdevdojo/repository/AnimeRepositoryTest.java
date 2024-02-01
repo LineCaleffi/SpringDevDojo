490a9ae3482ef0c585b1edf2007ddc608d50b70c
DESKTOP-UV7KBFV\Jeean <alinecaleffi97@gmail.com>
2024-02-01 14:45:03 -0300

    JUnit part 1

---
+++ src/test/java/com/teste/springdevdojo/repository/AnimeRepositoryTest.java
@@ -0,0 +1,33 @@
+package com.teste.springdevdojo.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.teste.springdevdojo.domain.Anime;
+
+@DataJpaTest
+//@DisplayName("AnimeRepositoryTest")
+class AnimeRepositoryTest {
+	@Autowired
+	private AnimeRepository animeRepository;
+
+	@Test
+	@DisplayName("Anime salvo com sucesso")
+	void saveSuccess() {
+		Anime anime = createAnime();
+		Anime animeSaved = this.animeRepository.save(anime);
+		
+		//  as Assertions servem para testar se o seu código esta de acordo com as regras que deve seguir
+		Assertions.assertThat(animeSaved).isNotNull();
+		Assertions.assertThat(animeSaved.getId()).isNotNull();
+        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeSaved.getName());	
+    }
+	
+
+	private Anime createAnime() {
+		return Anime.builder().name("Naruto").build();
+	}
+}
