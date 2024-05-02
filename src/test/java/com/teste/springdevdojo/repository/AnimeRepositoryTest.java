47cb2cc3a576b832066d139e52544f66c1f696f2
DESKTOP-UV7KBFV\Jeean <alinecaleffi97@gmail.com>
2024-05-02 14:20:46 -0300

    organizando codigo

--- src/test/java/com/teste/springdevdojo/repository/AnimeRepositoryTest.java
+++ src/test/java/com/teste/springdevdojo/repository/AnimeRepositoryTest.java
@@ -3,8 +3,6 @@ package com.teste.springdevdojo.repository;
 import java.util.List;
 import java.util.Optional;
 
-import javax.validation.ConstraintViolationException;
-
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -23,68 +21,68 @@ class AnimeRepositoryTest {
 	void saveSuccess() {
 		Anime anime = createAnime();
 		Anime animeSaved = this.animeRepository.save(anime);
-		
-		//  as Assertions servem para testar se o seu código esta de acordo com as regras que deve seguir
+
+		// as Assertions servem para testar se o seu código esta de acordo com as regras
+		// que deve seguir
 		Assertions.assertThat(animeSaved).isNotNull();
 		Assertions.assertThat(animeSaved.getId()).isNotNull();
-        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeSaved.getName());	
-    }
-	
+		Assertions.assertThat(animeSaved.getName()).isEqualTo(animeSaved.getName());
+	}
+
 	@Test
 	@DisplayName("Anime ALTERADO com sucesso")
 	void updateSuccess() {
 		Anime anime = createAnime();
 		Anime animeSaved = this.animeRepository.save(anime);
-		
+
 		animeSaved.setName("Avatar: A Lenda de AANG");
-		
+
 		Anime animeUpdate = this.animeRepository.save(animeSaved);
-		
-		//  as Assertions servem para testar se o seu código esta de acordo com as regras que deve seguir
-		Assertions.assertThat(animeSaved).isNotNull();
-		Assertions.assertThat(animeSaved.getId()).isNotNull();
-        Assertions.assertThat(animeSaved.getName()).isEqualTo(animeSaved.getName());	
-    }
-	
+
+		// as Assertions servem para testar se o seu código esta de acordo com as regras
+		// que deve seguir
+		Assertions.assertThat(animeUpdate).isNotNull();
+		Assertions.assertThat(animeUpdate.getId()).isNotNull();
+		Assertions.assertThat(animeUpdate.getName()).isEqualTo(animeSaved.getName());
+	}
+
 	@Test
 	@DisplayName("Anime Deletado com sucesso")
 	void deleteSuccess() {
 		Anime anime = createAnime();
 		Anime animeSaved = this.animeRepository.save(anime);
-		
+
 		this.animeRepository.delete(animeSaved);
 		this.animeRepository.findById(animeSaved.getId());
-		
+
 		Optional<Anime> animeOp = this.animeRepository.findById(animeSaved.getId());
 		Assertions.assertThat(animeOp.isEmpty()).isTrue();
-		
-    }
-	
+
+	}
+
 	@Test
 	@DisplayName("Find by name success")
 	void findByName_returnList_Success() {
 		Anime anime = createAnime();
 		Anime animeSaved = this.animeRepository.save(anime);
-		
+
 		String name = animeSaved.getName();
 		List<Anime> animes = this.animeRepository.findByName(name);
-		
+
 		Assertions.assertThat(animes).isNotEmpty();
 		Assertions.assertThat(animes).contains(animeSaved);
-    }
-	
+	}
+
 	@Test
 	@DisplayName("Retorna uma lista vazia quando nenhum anime for encontrado")
 	void findByName_returnList_NotFound() {
-        List<Anime> animes = this.animeRepository.findByName("xx");
+		List<Anime> animes = this.animeRepository.findByName("xx");
 
-        Assertions.assertThat(animes).isEmpty();
-    } // aula 31
-	
+		Assertions.assertThat(animes).isEmpty();
+	} // aula 31
+
 	private Anime createAnime() {
 		return Anime.builder().name("Naruto").build();
 	}
-	
-	
-	
+
 }
